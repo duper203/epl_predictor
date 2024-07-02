d1da292c95cb51dd5edd4d04a2d2f09872cc0f12
duper <hsookim203@gmail.com>
2024-07-02 16:33:17 +0900

    [FIX] streamlit

--- streamlit.py
+++ streamlit.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 ##---TITLE-----------------------------------------------------------------------------------## 
 st.title("EPL PREDICTOR")
-st.header('This is a header with a divider', divider='rainbow')
+st.header('', divider='rainbow')
 
 ##---CHART-----------------------------------------------------------------------------------## 
 
 st.header('Probabilities of Teams Achieving Each Rankings in the League')
 
-file_path = './simulation_data _step6/rank_probabilities.csv'
-probabilities_df = pd.read_csv(file_path)
+prob_file_path = './simulation_data _step6/step6_rank_probabilities.csv'
+probabilities_df = pd.read_csv(prob_file_path)
 
 # Extract team names
 teams = probabilities_df.iloc[:, 0].repeat(20).values
@@ -33,6 +33,7 @@ st.bar_chart(chart_data, x="ranking", y="probability", color="teams")
 
 
 ##---CHART-----------------------------------------------------------------------------------## 
+st.header('Average points after simmulating 1000times')
 
 df_avg_points = pd.read_csv("step6_average_points.csv")
 st.data_editor(
